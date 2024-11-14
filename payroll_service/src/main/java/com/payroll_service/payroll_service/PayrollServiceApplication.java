370630597bb769ebe7cb20f342deb4af9205e7c7
Glauber-Araujo <dev02@vrupi.com>
2024-11-14 16:44:09 -0300

    #20

--- payroll_service/src/main/java/com/payroll_service/payroll_service/PayrollServiceApplication.java
+++ payroll_service/src/main/java/com/payroll_service/payroll_service/PayrollServiceApplication.java
@@ -2,8 +2,10 @@ package com.payroll_service.payroll_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PayrollServiceApplication {
 
 	public static void main(String[] args) {
