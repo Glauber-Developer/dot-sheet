70941b0b5ce71ea0c3e848fd01b635793b4b9761
fxgvcv <gabriel.fogaca@edu.pucrs.br>
2024-11-14 18:40:59 -0300

    #1

--- api_gateway/src/main/java/com/api_gateway/api_gateway/ApiGatewayApplication.java
+++ api_gateway/src/main/java/com/api_gateway/api_gateway/ApiGatewayApplication.java
@@ -2,12 +2,26 @@ package com.api_gateway.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
 
+    @Bean
+    public GlobalFilter customFilter() {
+        return (exchange, chain) -> {
+            System.out.println("Global Pre Filter executed");
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                System.out.println("Global Post Filter executed");
+            }));
+        };
+    }
 }
