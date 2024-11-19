869c6a5b045d40adce876f99029eacb88e06f76b
fxgvcv <gabriel.fogaca@edu.pucrs.br>
2024-11-19 18:27:23 -0300

    Implement the gateway authentication

---
+++ api_gateway/src/main/java/com/api_gateway/api_gateway/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,45 @@
+package com.api_gateway.api_gateway.filter;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+
+@Component
+public class JwtAuthenticationFilter implements WebFilter {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        if (!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+            return chain.filter(exchange);
+        }
+
+        String token = request.getHeaders().getOrEmpty(HttpHeaders.AUTHORIZATION).get(0);
+        if (token == null || !token.startsWith("Bearer ")) {
+            return chain.filter(exchange);
+        }
+
+        token = token.substring(7);
+        try {
+            Claims claims = Jwts.parser()
+                    .setSigningKey(secretKey)
+                    .parseClaimsJws(token)
+                    .getBody();
+            exchange.getRequest().mutate().header("user-id", claims.getSubject()).build();
+        } catch (Exception e) {
+            return chain.filter(exchange);
+        }
+
+        return chain.filter(exchange);
+    }
+}
