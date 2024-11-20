bb9df11da64b101b71a1d1ee4891aaf9c652f935
Joao Vogel <joao.vogel@edu.pucrs.br>
2024-11-20 16:47:08 -0300

    estrutura criada

---
+++ attendance_service/src/main/java/com/attendance_service/modal/Attendance.java
@@ -0,0 +1,55 @@
+package com.attendance_service.modal;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class Attendance {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String employeeId;
+
+    @Column(nullable = false)
+    private LocalDateTime checkInTime;
+
+    @Column
+    private LocalDateTime checkOutTime;
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public LocalDateTime getCheckInTime() {
+        return checkInTime;
+    }
+
+    public void setCheckInTime(LocalDateTime checkInTime) {
+        this.checkInTime = checkInTime;
+    }
+
+    public LocalDateTime getCheckOutTime() {
+        return checkOutTime;
+    }
+
+    public void setCheckOutTime(LocalDateTime checkOutTime) {
+        this.checkOutTime = checkOutTime;
+    }
+}
