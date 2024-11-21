206d8fb8dc63060dcabb129424478e107354e8be
Glauber-Araujo <dev02@vrupi.com>
2024-11-21 01:10:48 -0300

    fix(attendance): #19 #20 #21

--- attendance_service/src/main/resources/data.sql
+++ attendance_service/src/main/resources/data.sql
@@ -1,11 +1,11 @@
-CREATE TABLE attendances (
-    id SERIAL PRIMARY KEY,
-    userId INT NOT NULL,
-    status VARCHAR(255) NOT NULL,
-    date DATE NOT NULL,
-    FOREIGN KEY (userId) REFERENCES users(id)
-); 
+-- CREATE TABLE attendances (
+--     id SERIAL PRIMARY KEY,
+--     userId INT NOT NULL,
+--     status VARCHAR(255) NOT NULL,
+--     date DATE NOT NULL,
+--     FOREIGN KEY (userId) REFERENCES users(id)
+-- ); 
 
 
-INSERT INTO ATTENDANCE (userId, status, date) VALUES ('1', 'Present', '2024-11-01');
-INSERT INTO ATTENDANCE (userId, status, date) VALUES ('2', 'Absent', '2024-11-01');
+-- INSERT INTO ATTENDANCE (userId, status, date) VALUES ('1', 'Present', '2024-11-01');
+-- INSERT INTO ATTENDANCE (userId, status, date) VALUES ('2', 'Absent', '2024-11-01');
