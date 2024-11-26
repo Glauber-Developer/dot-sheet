9e1765900a3ef887a61a726ce2756ab3e6f66b48
Glauber-Araujo <dev02@vrupi.com>
2024-11-26 14:24:30 -0300

    fix(cleanUser): clean #48

--- user_service/src/main/java/com/user_service/user_service/RabbitMQConfig.java
+++ user_service/src/main/java/com/user_service/user_service/RabbitMQConfig.java
@@ -4,10 +4,7 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
-// import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-// import org.springframework.amqp.rabbit.core.RabbitTemplate;
-// import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-// import org.springframework.amqp.support.converter.MessageConverter;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -40,34 +37,4 @@ public class RabbitMQConfig {
     Binding bindingPayroll(Queue userPayrollQueue, TopicExchange userExchange) {
         return BindingBuilder.bind(userPayrollQueue).to(userExchange).with("user.payroll.events");
     }
-
-    // public static final String EXCHANGE_NAME = "user_service_exchange";
-    // public static final String ROUTING_KEY = "user.events";
-
-    // @Bean
-    // public Queue reportingQueue() {
-    //     return new Queue("reporting_service_queue", false);
-    // }
-
-    // @Bean
-    // public TopicExchange exchange() {
-    //     return new TopicExchange(EXCHANGE_NAME);
-    // }
-
-    // @Bean
-    // public Binding binding(Queue reportingQueue, TopicExchange exchange) {
-    //     return BindingBuilder.bind(reportingQueue).to(exchange).with(ROUTING_KEY);
-    // }
-
-    // @Bean
-    // public MessageConverter jsonMessageConverter() {
-    //     return new Jackson2JsonMessageConverter();
-    // }
-
-    // @Bean
-    // public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
-    //     RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-    //     rabbitTemplate.setMessageConverter(jsonMessageConverter());
-    //     return rabbitTemplate;
-    // }
 }
